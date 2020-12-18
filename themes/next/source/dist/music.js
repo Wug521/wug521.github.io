134609fe39312f8afcc044d48301c77e4c6042d3
你是那个谁灬 <qiyi228535106@163.com>
2020-12-18 14:00:54 +0800

    修改配置文件信息

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -4,52 +4,22 @@ const ap = new APlayer({
     autoplay: true,
     audio: [
       {
-        name: "光年之外",
-        artist: 'G.E.N&邓紫棋',
-        url: 'http://www.ytmp3.cn/down/56591.mp3',
-        cover: 'http://img.ytmp3.cn/image/55.jpg?param=130y130',
+        name: "梦伴",
+        artist: '李悦君Ericaceae',
+        url: 'https://www.joy127.com/url/76855.mp3',
+        cover: 'https://s3.ax1x.com/2020/12/18/rJaMwD.jpg',
       },
       {
         name: 'Love Story',
         artist: 'Taylor Swift',
-        url: 'http://www.ytmp3.cn/down/49670.mp3',
+        url: 'https://www.joy127.com/url/71680.mp3',
         cover: 'https://github.com/Wug521/image/blob/master/2019-01-31/timg.jpg?raw=true?param=130y130',
       },
       {
-        name: '我已经爱上你',
-        artist: '九局',
-        url: 'http://www.ytmp3.cn/down/53633.mp3',
-        cover: 'http://p1.music.126.net/Bl1hEdJbMSj5YJsTqUjr-w==/109951163520311175.jpg?param=130y130',
-      },
-      {
-        name: '一人饮酒醉',
-        artist: 'MC文儿',
-        url: 'http://www.ytmp3.cn/down/37577.mp3',
-        cover: 'https://github.com/Wug521/image/blob/master/2019-01-31/wener.jpg?raw=true?param=130y130',
-      },
-      {
-        name: 'That Girl',
-        artist: 'Olly Murs',
-        url: 'http://www.ytmp3.cn/down/47869.mp3',
-        cover: 'http://img.ytmp3.cn/image/51.jpg?param=130y130',
-      },
-      {
-        name: 'Fade',
-        artist: 'Alan Walker',
-        url: 'http://www.ytmp3.cn/down/50556.mp3',
-        cover: 'http://img.ytmp3.cn/image/12.jpg?param=130y130',
-      },
-      {
-        name: 'havana',
-        artist: 'young thug&camila cabello',
-        url: 'http://www.ytmp3.cn/down/43802.mp3',
-        cover: 'http://img.ytmp3.cn/image/39.jpg?param=130y130',
-      },
-      {
-        name: 'nevada',
-        artist: 'vicetone&cozi zuehlsdorff',
-        url: 'http://www.ytmp3.cn/down/43421.mp3',
-        cover: 'http://img.ytmp3.cn/image/22.jpg?param=130y130',
+        name: '你莫走',
+        artist: '山水组合',
+        url: 'https://www.joy127.com/url/77226.mp3',
+        cover: 'https://s3.ax1x.com/2020/12/18/rJUmVg.jpg',
       }
     ]
 });
